56b24a720ba4aa77403e9f426f25ef65e86d2947
milosmarojevic <milos.marojevic20@gmail.com>
2026-09-03 09:03:10 +0200

    Changes to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+/* import "core-js/stable";
+import "regenerator-runtime/runtime"; */
 
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
